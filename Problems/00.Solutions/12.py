2faf4299de850a3f906f0df29ed7a9710cbc0a87
Yashwanth S <83501889+nameisyashwanth@users.noreply.github.com>
2021-06-07 18:40:55 +0530

    Create 12.py

---
+++ Problems/00.Solutions/12.py
@@ -0,0 +1,5 @@
+strg="I know what it's like to lose. To feel so desperately that you're right, yet to fail nonetheless.It’s frightening. Turns the legs to jelly. I ask you, to what end.Dread it. Run from it. Destiny arrives all the same. And now, it's here. Or should I say, I am."
+list1=strg.split()
+print(list1)
+strg1=" ".join(list1)
+print(strg1)
