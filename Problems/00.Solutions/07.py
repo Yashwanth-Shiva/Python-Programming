3be456595cbea4e4ee6c939dddd0c0bf764db6bd
Yashwanth S <83501889+nameisyashwanth@users.noreply.github.com>
2021-06-07 18:17:38 +0530

    Create 07.py

---
+++ Problems/00.Solutions/07.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+if(n1>100 and n2>100):
+    print("Both",n1,"and",n2,"are greater than 100")
+else:
+    print("Lesser than 100")
