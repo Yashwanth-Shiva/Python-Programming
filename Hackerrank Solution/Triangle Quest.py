eead1a05e8360a78904f9400c8251ac758c9071c
Yashwanth S <83501889+Yashwanth-Shiva@users.noreply.github.com>
2021-06-27 11:30:37 +0530

    Create Triangle Quest.py

---
+++ Hackerrank Solution/Triangle Quest.py
@@ -0,0 +1,11 @@
+for i in range(1,int(input())):
+    print((10**(i)//9)*i
+"""
+example:  i=3
+          10**3//9=111
+          111*3=333
+ """
+#Other method but this answer is not accepted by Hackerrank
+for i in range(1,int(input())):
+    print(str(i)*i)
+  
