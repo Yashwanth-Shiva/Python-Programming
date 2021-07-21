af5c60588baaee2059dbc456d901baf0143237db
Yashwanth S <83501889+Yashwanth-Shiva@users.noreply.github.com>
2021-07-21 15:47:41 +0530

    Update Company Logo.py

--- Hackerrank Solution/Company Logo.py
+++ Hackerrank Solution/Company Logo.py
@@ -1,11 +1,8 @@
 s=input()
-list1=list(s)
+list1=sorted(list(s))
 dict1=dict()
-dict2=dict()
 for i in list1:
     dict1[i]=dict1.get(i,0)+1
-#First sorting it in alphabetical order and then based on occurence
-dict2=dict(sorted(dict1.items(), key=lambda item: item[0]))
-x=tuple(sorted(dict2.items(), key=lambda item: item[1],reverse=True))
+x=tuple(sorted(dict1.items(), key=lambda item: item[1],reverse=True))
 for i in range(3):
     print(x[i][0],x[i][1])
