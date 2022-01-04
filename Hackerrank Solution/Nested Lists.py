3120cab749fe763722262e8a952663250df9cb3c
Yashwanth S <83501889+yash1th-s@users.noreply.github.com>
2022-01-04 17:03:21 +0530

    Update Nested Lists.py

--- Hackerrank Solution/Nested Lists.py
+++ Hackerrank Solution/Nested Lists.py
@@ -8,3 +8,19 @@ for _ in range(int(input())):
     list2.append(list1)
 sec_high=sorted(set([score for name,score in list2]))[1]
 print("\n".join(sorted([name for name,score in list2 if score==sec_high])))
+
+
+
+
+l1=[]
+l2=[]
+for i in range(int(input())):
+    name=input()
+    score=float(input())
+    l1=[name,score]
+    l2.append(l1)
+    
+second_highest=sorted(list(set(score for name,score in l2)))[1]
+for x,y in sorted(l2):
+    if y==second_highest:
+        print(x)
